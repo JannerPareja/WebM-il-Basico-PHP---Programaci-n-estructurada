1f064da427d7fed9938d1045ed7e7547660f19c3
Janner Pareja <janpar81@gmail.com>
2020-12-04 11:18:33 -0500

    Update head.php
    Se agregó el favicon..

--- views/front/templates/head.php
+++ views/front/templates/head.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	 <meta charset="utf-8">
+    <meta charset="utf-8">
+    <link rel="icon" href="http://localhost/webmail/public/images/email.png" type="image/png" sizes="16x16">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="stylesheet" type="text/css" href="http://localhost/webmail/public/css/style.css">
